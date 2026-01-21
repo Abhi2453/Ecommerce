e1ccaacd695732eb1e17704eae11d4deb0361602
Abhi2453 <162627707+Abhi2453@users.noreply.github.com>
2026-01-21 13:03:07 +0530

    Update server.js added origin url and listen 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // ✅ CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://ecommerce-qw5h.onrender.com', 'http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -46,6 +46,6 @@ app.use((err, req, res, next) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
