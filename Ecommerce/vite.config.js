87867ed5329a0b68362e7d823a12799b6687b32f
Abhi2453 <162627707+Abhi2453@users.noreply.github.com>
2026-01-21 05:47:56 +0530

    Update vite.config.js

--- Ecommerce/vite.config.js
+++ Ecommerce/vite.config.js
@@ -5,15 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    },
-  },
 })
